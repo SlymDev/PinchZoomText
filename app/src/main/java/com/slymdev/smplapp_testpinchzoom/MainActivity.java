4adf1184d67ca7c40c255f14b92195e79883f527
SlymDev <slymdev@gmx.com>
2015-10-07 00:28:43 -0700

    Initial commit

---
+++ app/src/main/java/com/slymdev/smplapp_testpinchzoom/MainActivity.java
@@ -0,0 +1,76 @@
+package com.slymdev.smplapp_testpinchzoom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+    final static float STEP = 200;
+    TextView textView;
+    float mRatio = 1.0f;
+    int mBaseDist;
+    float mBaseRatio;
+    float fontsize = 13;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.testZoom);
+        textView.setTextSize(mRatio + fontsize);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getPointerCount() == 2) {
+            int action = event.getAction();
+            int pureaction = action & MotionEvent.ACTION_MASK;
+            if (pureaction == MotionEvent.ACTION_POINTER_DOWN) {
+                mBaseDist = getDistance(event);
+                mBaseRatio = mRatio;
+            } else {
+                float delta = (getDistance(event) - mBaseDist) / STEP;
+                float multi = (float) Math.pow(2, delta);
+                mRatio = Math.min(1024.0f, Math.max(0.1f, mBaseRatio * multi));
+                textView.setTextSize(mRatio + 13);
+            }
+        }
+        return true;
+    }
+
+    int getDistance(MotionEvent event) {
+        int dx = (int) (event.getX(0) - event.getX(1));
+        int dy = (int) (event.getY(0) - event.getY(1));
+        return (int) (Math.sqrt(dx * dx + dy * dy));
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
